a9c328b6f60f2daf38be0cbdb82f0452026650cd
auliaansaa <146051193+auliaansaa@users.noreply.github.com>
2023-10-31 13:23:29 +0800

    kuliah algo 31/10/23

---
+++ 00-template/src/App.java
@@ -0,0 +1,33 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Program Biodata Mahasiswa
+        // Deklarasi variabel
+        String nama;
+        String nim;
+        String tgl_lahir;
+        String alamat;
+        float berat_badan;
+        float tinggi_badan;
+
+        // mengisi variabel
+        nama = "auliaa";
+        nim = "23241067";
+        tgl_lahir = "18 agustus 2005";
+        alamat = "lombok tengah";
+        berat_badan = 40f;
+        tinggi_badan = 170;
+
+        //cetak biodata 
+        System.out.println("==================");
+        System.out.println("Nama : " + nama);
+        System.out.println("Nim : " + nim);
+        System.out.println("Tanggal lahir : " + tgl_lahir);
+        System.out.println("Alamat :" + alamat);
+        System.out.println("Berat badan : " + berat_badan);
+        System.out.println("Tinggi badan : " + tinggi_badan);
+        System.out.println("==================");
+
+
+
+    }
+}
